a234084700f4ab6f529ac661f96ba12f6d8565a8
Silvio <stoikov126@gmail.com>
2023-10-25 17:25:05 +0300

    Added API files

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/UniverseApplication.java
@@ -0,0 +1,13 @@
+package com.unidev.universe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniverseApplication.class, args);
+	}
+
+}
---
+++ UniVerseAPI/universe/src/test/java/com/unidev/universe/UniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unidev.universe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
