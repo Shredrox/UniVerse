272a74a86658a4c57b432839085dfe8527a3ad99
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-11-18 12:30:59 +0200

    Remove BrowserRouter from main.jsx

--- UniVerseUI/universe-ui/src/main.jsx
+++ UniVerseUI/universe-ui/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import './styles/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
