3ced37d83490d2489e2b68a310fc2079608db514
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-11-18 12:40:33 +0200

    Update App to use createBrowserRouter, Layout and RouterProvider

--- UniVerseUI/universe-ui/src/App.jsx
+++ UniVerseUI/universe-ui/src/App.jsx
@@ -1,6 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import { Home } from "./pages/Home"
-import { Sidebar } from './components/Sidebar'
 import { News } from './pages/News'
 import { Courses } from './pages/Courses'
 import { Jobs } from './pages/Jobs'
@@ -8,30 +7,30 @@ import { Events } from './pages/Events'
 import { Groups } from './pages/Groups'
 import { Chats } from './pages/Chats'
 import { Settings } from './pages/Settings'
-import { Topbar } from './components/Topbar'
-import { UserPanel } from './components/UserPanel'
+import { Landing } from './pages/Landing'
+import { MainLayout } from './layouts/MainLayout'
 
 function App() {
+  const check = true;
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={check ? <MainLayout /> : <Landing/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/news" element={<News/>}/>
+        <Route path="/courses" element={<Courses/>}/>
+        <Route path="/jobs" element={<Jobs/>}/>
+        <Route path="/events" element={<Events/>}/>
+        <Route path="/groups" element={<Groups/>}/>
+        <Route path="/chats" element={<Chats/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+      </Route>   
+    )
+  );
 
   return (
     <div className='app-container'>
-      <Sidebar/>
-      <div className='wrapper'>
-        <Topbar/>
-        <main id='main-container'>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/news" element={<News/>}/>
-            <Route path="/courses" element={<Courses/>}/>
-            <Route path="/jobs" element={<Jobs/>}/>
-            <Route path="/events" element={<Events/>}/>
-            <Route path="/groups" element={<Groups/>}/>
-            <Route path="/chats" element={<Chats/>}/>
-            <Route path="/settings" element={<Settings/>}/>
-          </Routes>
-        </main>
-      </div>
-      <UserPanel/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
