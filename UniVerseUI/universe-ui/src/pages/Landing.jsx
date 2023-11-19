971a17e4f615a19b7344f467b0490bfb0445c223
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-11-19 09:17:39 +0200

    Update Landing.jsx

--- UniVerseUI/universe-ui/src/pages/Landing.jsx
+++ UniVerseUI/universe-ui/src/pages/Landing.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import UniVerseLogo from '../assets/images/logo-universe.png'
+import { AccessForm } from '../components/AccessForm'
 
-export const Landing = () => {
+export const Landing = ({setLoggedIn}) => {
   return (
-    <div>Landing</div>
+    <div className='landing-container'>
+      <div className='landing-logo-container'>
+        <img src={UniVerseLogo} alt="UniVerseLogo" />
+        UniVerse
+        <span className='landing-subtitle'>Connecting Minds, Building Futures</span>
+      </div>
+      <AccessForm setLoggedIn={setLoggedIn}/>
+    </div>
   )
 }
