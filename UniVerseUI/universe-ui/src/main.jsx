d03109b38d52ab998dd918f23cc9d81bb5ec735c
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-11-19 20:02:36 +0200

    Wrap app with AuthProvider

--- UniVerseUI/universe-ui/src/main.jsx
+++ UniVerseUI/universe-ui/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 import './styles/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 )
