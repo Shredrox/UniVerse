828e43bf6101c5f590191a9b35798983b5a6e6e3
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-11-21 21:08:24 +0200

    Add React Query Devtools to main.jsx

--- UniVerseUI/universe-ui/src/main.jsx
+++ UniVerseUI/universe-ui/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import './styles/style.css'
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <App />
+        <ReactQueryDevtools initialIsOpen={false}/>
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
