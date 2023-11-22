df80a3144e9fe5b58bf6e7f76dd4bd4f1ee2d6f5
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-11-22 17:04:46 +0200

    Update Home.jsx

--- UniVerseUI/universe-ui/src/pages/Home.jsx
+++ UniVerseUI/universe-ui/src/pages/Home.jsx
@@ -3,13 +3,7 @@ import { Feed } from '../components/Feed'
 
 export const Home = () => {
   return (
-    <div id='home-page'>
-      <div className='feed-top'>
-        Feed
-        <div className='create-post'>
-            Create post
-        </div>
-      </div>
+    <div>
       <Feed/>
     </div>
   )
