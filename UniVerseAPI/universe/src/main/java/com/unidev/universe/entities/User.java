a1b4b283ae7b5ff848277eb2637ba809eb43793f
Valentin Hadzhiminov <35624036+ValentinH1810@users.noreply.github.com>
2023-11-26 18:05:04 +0200

    Create User entity.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/User.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/User.java
@@ -1,4 +1,22 @@
 package com.unidev.universe.entities;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
 }
