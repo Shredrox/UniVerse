60712860312a161f0d02bfb9746bb40f1ee90771
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-11-26 18:06:35 +0200

    Remove testing data, incorporate React Query for post data fetching and add CreatePostForm to Feed

--- UniVerseUI/universe-ui/src/components/Feed.jsx
+++ UniVerseUI/universe-ui/src/components/Feed.jsx
@@ -1,31 +1,71 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Post } from './Post'
+import { getPosts, addPost } from '../api/postsApi';
+import { useState } from 'react';
+import CreatePostForm from './CreatePostForm';
 
 export const Feed = () => {
-  const posts = [
-    {
-      id: 1,
-      authorId: 1,
-      title: "Test",
-      content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit fahfdh  jatrj aet weg janw nhjaer haejrn jaenrg aerng jerang jaen aenhearjn hanerj hnyhaejrnh janerkjhaeskjrhn akejrnh kajen",
-      image: null
+  const queryClient = useQueryClient();
+
+  const {data: posts, isLoading, isError, error} = useQuery({ 
+    queryKey: ["posts"],
+    queryFn: () => getPosts(),
+  });
+
+  const {mutateAsync: addPostMutation} = useMutation({
+    mutationFn: addPost,
+    onSuccess: () =>{
+      queryClient.invalidateQueries(["posts"]);
     },
-    {
-      id: 2,
-      authorId: 2,
-      title: "Test 2",
-      content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit fahfdh  jatrj aet weg janw nhjaer haejrn jaenrg aerng jerang jaen aenhearjn hanerj hnyhaejrnh janerkjhaeskjrhn akejrnh kajen",
-      image: 'src/assets/images/background-landing-page.jpg'
+  });
+
+  const [creatingPost, setCreatingPost] = useState(false);
+
+  const createPost = async (title, content, image) =>{
+    try{
+      const data = new FormData();
+      data.append('title', title);
+      data.append('content', content);
+      data.append('image', image);
+      data.append('authorId', 2);
+      console.log(data);
+
+      const response = await addPostMutation(data);
+      console.log(response);
+    }catch(e){
+      console.error(e.response);
     }
-  ];
+  }
+
+  if(isError){
+    return <div>{error.message}</div>
+  }
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
 
   return (
-    <div className='feed'>
-      {posts?.map(post =>
-        <div key={post.id} className='post-container'>
-          <div className='line'>&nbsp;</div>
-          <Post post={post}/>
-        </div>
-      )}
+    <div className='feed-container'>
+      <div className='feed-top'>
+        {creatingPost ? "Creating Post" : "Feed"} 
+        <button 
+          className={creatingPost ? 'cancel-button' : 'confirm-button'} 
+          onClick={() => setCreatingPost(!creatingPost)}>
+            {creatingPost ? "Cancel" : "Create Post"}
+        </button>
+      </div>
+      {creatingPost ? <CreatePostForm setCreatingPost={setCreatingPost} createPost={createPost}/> 
+      :
+      <div className='feed'>
+        {posts?.map(post =>
+          <div key={post.id} className='post-container'>
+            <div className='line'>&nbsp;</div>
+            <Post post={post}/>
+          </div>
+        )}
+      </div>
+      }
     </div>
   )
 }
