3bfc3dc6f669d34d9444bafe9e49fcb3e70225d6
Silvio Stoykov <stoikov126@gmail.com>
2023-11-28 11:27:40 +0200

    Create Comment entity.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/Comment.java
@@ -0,0 +1,35 @@
+package com.unidev.universe.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private int id;
+
+    @NotNull
+    @Column(name = "comment_desc")
+    private String description;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    Post post;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+}
