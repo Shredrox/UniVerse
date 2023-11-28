2f6ac78aa68f20cb732b0bb8b8deff3979b0326d
Silvio Stoykov <stoikov126@gmail.com>
2023-11-28 11:08:03 +0200

    Create Like entity.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/Like.java
@@ -0,0 +1,27 @@
+package com.unidev.universe.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "likes")
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private int likeId;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    Post post;
+}
