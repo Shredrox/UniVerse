35fb4af1a9552ba1b9e10e92b441770cc47bc209
Silvio Stoykov <stoikov126@gmail.com>
2023-11-29 11:41:49 +0200

    Add fields to RegisterRequest class.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/RegisterRequest.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/RegisterRequest.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    private String name;
+    private String lastName;
     private String email;
     private String password;
 }
