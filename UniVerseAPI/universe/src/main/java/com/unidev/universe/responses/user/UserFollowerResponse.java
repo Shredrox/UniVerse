8560c36b4ed82eb2147115e5c1a555debd502044
Silvio Stoykov <stoikov126@gmail.com>
2023-11-29 10:03:05 +0200

    Create UserFollowerResponse class.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/responses/user/UserFollowerResponse.java
@@ -0,0 +1,14 @@
+package com.unidev.universe.responses.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserFollowerResponse {
+    private int userId;
+    private String name;
+    private String lastName;
+}
