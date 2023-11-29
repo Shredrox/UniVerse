d451083200d0861d7e7fcfa39fb0951d47740d73
Silvio Stoykov <stoikov126@gmail.com>
2023-11-29 11:40:34 +0200

    Create RegisterRequest class.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.unidev.universe.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String password;
+}
