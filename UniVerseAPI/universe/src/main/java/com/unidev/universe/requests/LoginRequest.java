b2948e16306fd8d198324609b629cd79145399cf
Silvio Stoykov <stoikov126@gmail.com>
2023-11-29 11:39:11 +0200

    Create LoginRequest class.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.unidev.universe.requests;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+    private String name;
+    private String lastName;
+}
