899a53626d7ff1193de99a3ae532bda33c77cbb5
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-03 15:02:20 +0200

    Add job info and applying to job to JobsDetails page

--- UniVerseUI/universe-ui/src/pages/JobDetails.jsx
+++ UniVerseUI/universe-ui/src/pages/JobDetails.jsx
@@ -1,10 +1,44 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom"
+import { useAuth } from "../hooks/useAuth";
+import { applyToJob, getJobById } from "../api/jobsApi";
+import { IoBriefcase } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+import { HiBuildingOffice2 } from "react-icons/hi2";
 
 const JobDetails = () => {
   const { jobId } = useParams();
+  const { auth } = useAuth();
+
+  const {data: job, isLoading, isError, error} = useQuery({ 
+    queryKey: ["job", jobId],
+    queryFn: () => getJobById(jobId),
+  });
+
+  const {mutateAsync: applyToJobMutation} = useMutation({
+    mutationFn: applyToJob,
+  });
+
+  if(isError){
+    return <div>{error.message}</div>
+  }
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
 
   return (
-    <div>JobOfferDetails</div>
+    <div className="job-details-container">
+      <div className="job-details">
+        <h2>{job?.title}</h2>
+        <h3>{job?.company}</h3>
+        <span className="job-text-field"><HiBuildingOffice2 className="job-icon"/>{job?.requirements}</span>
+        <span className="job-text-field"><FaLocationDot className="job-icon"/>{job?.location}</span>
+        <span className="job-text-field"><IoBriefcase className="job-icon"/>{job?.type}</span>
+        <p className="job-description">{job?.description}</p>
+        <button onClick={() => applyToJobMutation({jobId: job.id, username: auth?.user})} className="confirm-button">Apply</button>
+      </div>
+    </div>
   )
 }
 
