6b3c7226182c3e3c69405723b8599c34dd495364
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-04 11:48:15 +0200

    Add attendEvent to TrendingEvent

--- UniVerseUI/universe-ui/src/components/TrendingEvent.jsx
+++ UniVerseUI/universe-ui/src/components/TrendingEvent.jsx
@@ -1,8 +1,13 @@
-const TrendingEvent = ({event}) => {
+const TrendingEvent = ({event, attendEvent}) => {
   return (
-    <div className='trending-event'>
-        <img src="https://picsum.photos/200/100" alt="EventImage" />
-        <h3>{event.title}</h3>
+    <div className='trending-event' >
+      <img src="https://picsum.photos/200/100" alt="EventImage" />
+      <h3>{event.title}</h3>
+      <button 
+        onClick={() => attendEvent(event.id)} 
+        className="confirm-button">
+          Attend
+      </button>
     </div>
   )
 }
