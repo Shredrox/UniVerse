33b7492c3883b6ad51188ff79215a4689f6e4c49
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-04 11:49:39 +0200

    Add queryClient and trending events query to Events page

--- UniVerseUI/universe-ui/src/pages/Events.jsx
+++ UniVerseUI/universe-ui/src/pages/Events.jsx
@@ -1,5 +1,5 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { attendEvent, getEvents } from '../api/eventsApi';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { attendEvent, getEvents, getTrendingEvents } from '../api/eventsApi';
 import Event from '../components/Event';
 import { MdEventAvailable } from "react-icons/md";
 import { IoTrendingUp } from "react-icons/io5";
@@ -8,29 +8,34 @@ import { useAuth } from "../hooks/useAuth";
 
 const Events = () => {
   const { auth } = useAuth();
+  const queryClient = useQueryClient();
 
   const {data: events, isLoading, isError, error} = useQuery({ 
     queryKey: ["events"],
     queryFn: () => getEvents(),
   });
 
+  const {data: trendingEvents, isLoading: trendingEventsIsLoading, isError: trendingEventsIsError, error: trendingEventsError} = useQuery({ 
+    queryKey: ["trendingEvents"],
+    queryFn: () => getTrendingEvents(),
+  });
+
   const {mutateAsync: attendEventMutation} = useMutation({
     mutationFn: attendEvent,
     onSuccess: () =>{
-      queryClient.invalidateQueries(["events"]);
+      queryClient.invalidateQueries(["events", "trendingEvents"]);
     },
   });
 
   const attendNewEvent = (eventId) =>{
-    console.log(eventId);
     attendEventMutation({eventId: eventId, username: auth?.user})
   }
 
-  if(isError){
-    return <div>{error.message}</div>
+  if(isError || trendingEventsIsError){
+    return <div>{error?.message}{trendingEventsError?.message}</div>
   }
 
-  if(isLoading){
+  if(isLoading || trendingEventsIsLoading){
     return <div>Loading...</div>
   }
 
@@ -39,15 +44,15 @@ const Events = () => {
       <div className="trending-events-container">
         <h2><IoTrendingUp className='event-icon'/>Trending Events</h2>
         <div className='trending-events-list'>
-          {events.map((event, index) =>
-            <TrendingEvent key={index} event={event}/>
+          {trendingEvents?.map((event, index) =>
+            <TrendingEvent key={index} event={event} attendEvent={attendNewEvent}/>
           )}
         </div>
       </div>
       <div className="events-list-container">
-      <h2><MdEventAvailable className='event-icon'/> Events</h2>
+      <h2><MdEventAvailable className='event-icon'/>Events</h2>
         <div className='events-list'>
-          {events.map((event, index) =>
+          {events?.map((event, index) =>
             <Event key={index} event={event} attendEvent={attendNewEvent}/>
           )}
         </div>
