be7494c21e028bb35f82aae3a9354c9f9f991fb7
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-04 16:55:35 +0200

    Add more post API requests to postsApi

--- UniVerseUI/universe-ui/src/api/postsApi.js
+++ UniVerseUI/universe-ui/src/api/postsApi.js
@@ -5,6 +5,33 @@ export const getPosts = async () =>{
     return response.data;
 }
 
+export const getFriendsPosts = async (username) =>{
+    const response = await axios.get('/Posts/GetAllPosts');
+    return response.data;
+}
+
+export const getPostById = async (id) =>{
+    const response = await axios.patch(`/Posts/${post.id}`, id);
+    return response.data;
+}
+
+export const getPostLikes = async (postId) =>{
+    return 69;
+}
+
+export const getIsLiked = async (postId, username) =>{
+    console.log(postId + " " + username);
+    return false;
+}
+
+export const likePost = async ({postId, username}) =>{
+    console.log(postId + " " + username + " like");
+}
+
+export const unlikePost = async ({postId, username}) =>{
+    console.log(postId + " " + username + " unlike");
+}
+
 export const addPost = async (post) =>{
     return await axios.post('/Posts/CreatePost', post);
 }
