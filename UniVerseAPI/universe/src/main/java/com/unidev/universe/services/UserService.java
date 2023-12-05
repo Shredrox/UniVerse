e612d00cf30d400e1955bb37fb61f57c9baf23e3
Silvio Stoykov <stoikov126@gmail.com>
2023-12-05 13:02:42 +0200

    Override loadUserByUsername method from UserDetailsService.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/services/UserService.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/services/UserService.java
@@ -1,4 +1,26 @@
 package com.unidev.universe.services;
 
-public class UserService {
+import com.unidev.universe.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            return userRepository.findByEmail(username)
+                    .orElseThrow(()->new AccountNotFoundException(String.format("User not found!", username)));
+        } catch (AccountNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
