8d80ae8780a57de0e1b7ee92a0cbfa7ed5cb1c60
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-05 18:20:36 +0200

    Add comment functionality to Post component

--- UniVerseUI/universe-ui/src/components/Post.jsx
+++ UniVerseUI/universe-ui/src/components/Post.jsx
@@ -2,10 +2,16 @@ import { useNavigate } from 'react-router-dom'
 import CommentIcon from '../assets/icons/icon-comment.svg'
 import { FaRegHeart, FaHeart  } from "react-icons/fa";
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { getIsLiked, getPostLikes, likePost, unlikePost } from '../api/postsApi';
+import { getIsLiked, getPostCommentCount, getPostLikes, likePost, unlikePost } from '../api/postsApi';
 import { useAuth } from '../hooks/useAuth'
+import CommentSection from './CommentSection';
+import { useState } from 'react';
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Post = ({post}) => {
+  const [isCommentSectionOn, setIsCommentSectionOn] = useState(false);
+
   const navigate = useNavigate();
   const { auth } = useAuth();
 
@@ -14,6 +20,11 @@ const Post = ({post}) => {
   const {data: postLikes} = useQuery({ 
     queryKey: ["postLikes", post.id],
     queryFn: () => getPostLikes(post.id),
+  });
+
+  const {data: postCommentCount} = useQuery({ 
+    queryKey: ["postCommentCount", post.id],
+    queryFn: () => getPostCommentCount(post.id),
   });
 
   const {data: isLiked, isLoading, isError, error} = useQuery({ 
@@ -35,28 +46,42 @@ const Post = ({post}) => {
     },
   });
 
+  const toggleComment = () =>{
+    setIsCommentSectionOn(!isCommentSectionOn);
+  }
+
   return (
-    <div className='post'>
-      <div className='post-author'>
-        <div className='author-profile-picture'></div>
-        <span onClick={() => navigate(`/profile/${post.authorName}`)}>{post.authorName}</span>
+    <div className='post-container'>
+      <div className='line'>&nbsp;</div>
+      <div className='post'>
+        <div className='post-author'>
+          <div className='author-profile-picture'></div>
+          <span onClick={() => navigate(`/profile/${post.authorName}`)}>{post.authorName}</span>
+        </div>
+        <div className='post-content'>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          {post.image && <img src={post.image} alt='postImage'/>} 
+        </div>
+        <div className='interaction-container'>
+          <span>
+            {isLiked ? 
+            <FaHeart onClick={() => likePostMutation({postId: post.id, username: auth?.user})} className='interaction-icon'/>
+            :
+            <FaRegHeart onClick={() => unlikePostMutation({postId: post.id, username: auth?.user})} className='interaction-icon'/>
+            }
+            {postLikes}
+          </span>
+          <span style={{cursor: "pointer", userSelect: "none"}} onClick={toggleComment} >
+            <img className='interaction-icon' src={CommentIcon}/>
+            {postCommentCount} Comments {isCommentSectionOn ? 
+            <IoIosArrowDown className='arrow-icon-down'/> 
+            : 
+            <IoIosArrowForward className='arrow-icon-forward'/>}
+          </span>
+        </div>
       </div>
-      <div className='post-content'>
-        <h3>{post.title}</h3>
-        <p>{post.content}</p>
-        {post.image && <img src={post.image} alt='postImage'/>} 
-      </div>
-      <div className='interaction-container'>
-        <span>
-          {isLiked ? 
-          <FaHeart onClick={() => likePostMutation({postId: post.id, username: auth?.user})} className='interaction-icon'/>
-          :
-          <FaRegHeart onClick={() => unlikePostMutation({postId: post.id, username: auth?.user})} className='interaction-icon'/>
-          }
-          {postLikes}
-        </span>
-        <span><img className='interaction-icon' src={CommentIcon}/>Comment</span>
-      </div>
+      {isCommentSectionOn && <CommentSection postId={post.id}/>}
     </div>
   )
 }
