08a8fb8fc7644d187e721c5ce1a45779c91f315b
Silvio Stoykov <stoikov126@gmail.com>
2023-12-05 16:07:08 +0200

    Create RegistrationController class.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/controllers/RegistrationController.java
@@ -0,0 +1,20 @@
+package com.unidev.universe.controllers;
+
+import com.unidev.universe.requests.RegistrationRequest;
+import com.unidev.universe.services.RegistrationService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+@AllArgsConstructor
+public class RegistrationController {
+    private RegistrationService registrationService;
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest req){
+        return registrationService.register(req);
+    }
+}
