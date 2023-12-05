7858b2134b72660869df6c1f64c3291c8d2ea48b
Silvio Stoykov <stoikov126@gmail.com>
2023-12-05 16:06:30 +0200

    Create RegistrationService class.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/services/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.unidev.universe.services;
+
+import com.unidev.universe.requests.RegistrationRequest;
+
+public class RegistrationService {
+    public String register(RegistrationRequest req) {
+        return null;
+    }
+}
