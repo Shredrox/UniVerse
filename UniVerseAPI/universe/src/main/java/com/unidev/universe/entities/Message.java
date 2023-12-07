9476f533bc43ff2543ac53a8a90533f98ffc6a95
Valentin Hadzhiminov <35624036+ValentinH1810@users.noreply.github.com>
2023-12-08 00:30:24 +0200

    Create Message entity.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/Message.java
@@ -0,0 +1,35 @@
+package com.unidev.universe.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "message")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private int id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "chat_id")
+    Chat chat;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @NotNull
+    @Column(name = "content")
+    private String content;
+}
