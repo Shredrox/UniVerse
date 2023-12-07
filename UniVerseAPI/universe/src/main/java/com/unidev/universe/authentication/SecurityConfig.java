5eaad989a5efd0b4834bfc302de85b4b5bcdd896
Silvio Stoykov <stoikov126@gmail.com>
2023-12-08 00:23:04 +0200

    Implementing SecurityConfigurer methods.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/authentication/SecurityConfig.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/authentication/SecurityConfig.java
@@ -1,18 +1,67 @@
 package com.unidev.universe.authentication;
-
+import com.unidev.universe.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.SecurityConfigurer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig implements SecurityConfigurer<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder encodedPassword;
+
+    public SecurityConfig(UserService userService, BCryptPasswordEncoder encodedPassword) {
+        this.userService = userService;
+        this.encodedPassword = encodedPassword;
+    }
+
+    @Override
+    public void init(HttpSecurity builder) throws Exception {
+
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v*/registration/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .cors().and().csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/registration")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(encodedPassword);
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
 }
