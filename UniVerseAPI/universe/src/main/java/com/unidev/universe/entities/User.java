4926a371345c42aba7d7ef2d423212b888a434e1
Valentin Hadzhiminov <35624036+ValentinH1810@users.noreply.github.com>
2023-12-08 00:29:56 +0200

    Change User entity name.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/User.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/User.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="users")
+@Table(name="user")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
