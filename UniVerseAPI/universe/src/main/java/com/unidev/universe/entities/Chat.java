842d08bfc4e7dbdbe98bad4a80c150a48702fcf9
Valentin Hadzhiminov <35624036+ValentinH1810@users.noreply.github.com>
2023-12-08 00:22:43 +0200

    Create Chat entity.

---
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/Chat.java
@@ -0,0 +1,37 @@
+package com.unidev.universe.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "chat")
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id")
+    private int id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user1;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user2;
+
+    @NotNull
+    @Column(name = "created_at")
+    Date createdAt;
+}
