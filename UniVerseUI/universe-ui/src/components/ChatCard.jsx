49c2a81470ea196e15115527fab4aac07eae4377
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-10 16:02:18 +0200

    Add check for the chat user to ChatCard

--- UniVerseUI/universe-ui/src/components/ChatCard.jsx
+++ UniVerseUI/universe-ui/src/components/ChatCard.jsx
@@ -1,8 +1,13 @@
 import { useLocation, useNavigate } from "react-router-dom"
+import { useAuth } from '../hooks/useAuth'
 
 const ChatCard = ({chat}) => {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const { auth } = useAuth();
+  const chatUser = auth?.user === chat.user1 ? chat.user2 : chat.user1;
+  const chatPath = `/chats/${chatUser}`;
 
   function isSelected(path){
     return location.pathname.includes(path) ? 'chat-card-active' : 'chat-card'
@@ -11,9 +16,10 @@ const ChatCard = ({chat}) => {
   return (
     <div className='chat-card-container'>
       <div className='chat-card-line'>&nbsp;</div>
-      <div className={isSelected(`/chats/${chat.user}`) }  onClick={() => navigate(`/chats/${chat.user}`)}>
+      <div className={isSelected(chatPath)}  
+        onClick={() => navigate(chatPath)}>
         <div className="chat-profile-picture"></div>
-        {chat.user}
+        {chatUser}
       </div>
     </div>
   )
