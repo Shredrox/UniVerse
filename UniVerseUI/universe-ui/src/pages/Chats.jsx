68dbde0594f1bec533b17d30c854690c76947c3d
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-10 10:59:35 +0200

    Add user chats query to Chats

--- UniVerseUI/universe-ui/src/pages/Chats.jsx
+++ UniVerseUI/universe-ui/src/pages/Chats.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import { useAuth } from '../hooks/useAuth'
+import ChatCard from '../components/ChatCard';
+import { getChats } from '../api/chatsApi';
 
-export const Chats = () => {
+const Chats = () => {
+  const { auth } = useAuth();
+
+  const {data: chats, isLoading, isError, error} = useQuery({ 
+    queryKey: ["chats", auth?.user],
+    queryFn: () => getChats(auth?.user),
+  });
+
+  if(isError){
+    return <div>{error.message}</div>
+  }
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
   return (
-    <div>Chats</div>
+    <div className='chats-list'>
+      {chats.length > 0 ?
+      chats?.map((chat, index) =>
+        <ChatCard key={index} chat={chat}/>
+      )
+      :
+      <div>No chats</div>
+      }
+    </div>
   )
 }
+
+export default Chats
