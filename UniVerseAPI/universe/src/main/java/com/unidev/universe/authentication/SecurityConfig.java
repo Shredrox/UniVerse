1abd2711120338cd27415ffb3bf640166a9fb5dd
Silvio <stoikov126@gmail.com>
2023-12-10 14:33:43 +0200

    Extend SecurityConfigurerAdapter in SecurityConfig class.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/authentication/SecurityConfig.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/authentication/SecurityConfig.java
@@ -1,66 +1,52 @@
 package com.unidev.universe.authentication;
+
 import com.unidev.universe.services.UserService;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.SecurityConfigurer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
-import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Configures Spring Security for a Spring Boot application.
+ * Permits unauthenticated access to "/api/v1/registration/**",
+ * requires authentication for other requests, and configures form-based login.
+ **/
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig implements SecurityConfigurer<DefaultSecurityFilterChain, HttpSecurity> {
-
+public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final UserService userService;
-    private final BCryptPasswordEncoder encodedPassword;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public SecurityConfig(UserService userService, BCryptPasswordEncoder encodedPassword) {
+    public SecurityConfig(UserService userService, BCryptPasswordEncoder passwordEncoder) {
         this.userService = userService;
-        this.encodedPassword = encodedPassword;
-    }
-
-    @Override
-    public void init(HttpSecurity builder) throws Exception {
-
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/v*/registration/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated().and()
-                .formLogin();
-    }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .cors().and().csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/registration")
-                .permitAll()
-                .anyRequest()
-                .authenticated().and()
-                .formLogin();
-        return http.build();
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> {
+            try {
+                auth.
+                        requestMatchers("/api/v1/registration/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                        .and().formLogin();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider =
-                new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(encodedPassword);
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(userService);
         return provider;
     }
