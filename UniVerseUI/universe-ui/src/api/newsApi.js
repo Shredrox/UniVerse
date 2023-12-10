b58901637be6aa653466d674a26a2dcedab43de7
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-10 16:50:05 +0200

    Add mock data for testing

--- UniVerseUI/universe-ui/src/api/newsApi.js
+++ UniVerseUI/universe-ui/src/api/newsApi.js
@@ -1,13 +1,30 @@
 import axios from "./axios";
 
-export const getNews = async () =>{
-    const response = await axios.get('/News/GetAllNews');
-    return response.data;
+// export const getNews = async () =>{
+//     const response = await axios.get('/News/GetAllNews');
+//     return response.data;
+// }
+
+// export const getNewsByTitle = async (title) =>{
+//     const response = await axios.get(`/News/${title}`);
+//     return response.data;
+// }
+
+export const getNews = () =>{
+    const newsList = [
+        { pinned: false, title: "Lorem ipsum dolor sit amethfermhlkaekjgnalkejrngkajernghfahjajfhhhhhhhhhhfhadfjhnafdljhnadlfnhljtujsrtjsrtjstrkadfnhkjadfnhkjlandflkjhnadflkjhnaldfkjhnadlkfjhnakjdfnhkajdfnhkjadnhuraenhuaerhjnerhnaeruhnaerjhnaerunhgdfjnhaerhafdjkhnalejrn", image: "https://picsum.photos/1000/500", date: "26.11.2023" },
+        { pinned: true, title: "nvnsvkajsdnglakj", image: "https://picsum.photos/700/400", date: "02.12.2023" },
+        { pinned: true, title: "nonvindivnsvkajsdnglakj", image: "https://picsum.photos/300/400", date: "01.12.2023" },
+        { pinned: false, title: "novini 2", image: "https://picsum.photos/600/400", date: "03.12.2023" },
+        { pinned: false, title: "novini 3", text: "aaenhlkjaernhsjtrsjjjjadfjgfdjsrtjkrstkjmsrlktjlksrtmhksrmthkmsrtlkhmkjmsrlktjlksrtmhksrmthkmsrkjmsrlktjlksrtmhksrmthkmsrkjmsrlktjlksrtmhksrmthkmsrkjmsrlktjlksrtmhksrmthkmsrkjmsrlktjlksrtmhksrmthkmsrkjmsrlktjlksrtmhksrmthkmsrkjmsrlktjlksrtmhksrmthkmsrsrtlkhmsrtlkhmrstlkmhsrtkmh;lskrtmhtlkrjjjjjjjjjjjjjjjjjjjjjjjjjjjjgsjfgjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjgsjfgjjjgsjfgjjjgsjfgjjjgsjfgjjjgsjfgjjjgsjfgjjjgsjfgjjjgsjfgjjjgsjfgjjjgsjfgjjjgsjfgkja", image: "https://picsum.photos/3840/2160", date: "01.12.2023" }
+    ];
+    
+    return newsList;
 }
 
 export const getNewsByTitle = async (title) =>{
-    const response = await axios.get(`/News/${title}`);
-    return response.data;
+    const result = getNews().find(news => news.title === title);
+    return result;
 }
 
 export const addNews = async (news) =>{
