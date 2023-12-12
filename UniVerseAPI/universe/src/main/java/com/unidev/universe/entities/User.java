6863061c4a7975ccc09b926daa34748a5c7c919f
Valentin Hadzhiminov <35624036+ValentinH1810@users.noreply.github.com>
2023-12-12 17:30:00 +0200

    Update User entity.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/User.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/User.java
@@ -24,6 +24,9 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "username",nullable=false, unique=true)
+    private String username;
+
     @Column(name = "email",nullable=false, unique=true)
     private String email;
 
