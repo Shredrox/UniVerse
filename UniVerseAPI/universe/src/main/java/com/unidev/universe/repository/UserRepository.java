fdc58d022f5addb680cf4f5ba3af5bb5fd21e4ed
Valentin Hadzhiminov <35624036+ValentinH1810@users.noreply.github.com>
2023-12-12 17:32:39 +0200

    Update UserRepository interface.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/repository/UserRepository.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/repository/UserRepository.java
@@ -5,4 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    User findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    void removeFriendByUsername(String loggedInUser, String profileUser);
+
+    void addFriendByUsername(String loggedInUser, String profileUser);
+
+    boolean checkFriendship(String usernameUser1, String usernameUser2);
 }
