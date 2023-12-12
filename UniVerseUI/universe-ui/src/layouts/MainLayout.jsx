a5bb2e17bd4d52519b85777c5a7ee00089d89873
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-12 17:51:36 +0200

    Remove ErrorBoundary from MainLayout

--- UniVerseUI/universe-ui/src/layouts/MainLayout.jsx
+++ UniVerseUI/universe-ui/src/layouts/MainLayout.jsx
@@ -3,8 +3,6 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 import { Topbar } from '../components/Topbar'
 import SocialPanel from '../components/SocialPanel'
-import { ErrorBoundary } from 'react-error-boundary'
-import ErrorFallback from "../components/fallbacks/ErrorFallback"
 
 export const MainLayout = () => {
   return(
@@ -13,9 +11,7 @@ export const MainLayout = () => {
       <div className='wrapper'>
         <Topbar/>
         <main id='main-container'>
-          <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <Outlet />
-          </ErrorBoundary>
+          <Outlet />
         </main>
       </div>
       <SocialPanel/>
