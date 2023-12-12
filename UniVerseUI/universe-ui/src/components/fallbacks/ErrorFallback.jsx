ff5eb650a11216569f79b52dd5b020f1e3e6a043
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-12 17:25:48 +0200

    Add resetBoundary, error icon and container to ErrorFallback

--- UniVerseUI/universe-ui/src/components/fallbacks/ErrorFallback.jsx
+++ UniVerseUI/universe-ui/src/components/fallbacks/ErrorFallback.jsx
@@ -1,6 +1,16 @@
+import { useErrorBoundary } from "react-error-boundary"
+import { MdErrorOutline } from "react-icons/md";
+
 const ErrorFallback = ({error}) => {
+  const { resetBoundary } = useErrorBoundary();
+
   return (
-    <div className='error-fallback'>Error: {error.message}</div>
+    <div className="error-fallback-container">
+      <div className='error-fallback'>
+        <span><MdErrorOutline className="error-icon"/> Error: {error.message}</span>
+        <button onClick={resetBoundary} className="confirm-button">Try Again</button>
+      </div>
+    </div>
   )
 }
 
