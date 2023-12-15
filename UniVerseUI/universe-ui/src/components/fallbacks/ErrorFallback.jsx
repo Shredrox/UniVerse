cc8d555d39c16d3f05b2027b00251ad594d39f19
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-15 13:01:18 +0200

    Update ErrorFallback

--- UniVerseUI/universe-ui/src/components/fallbacks/ErrorFallback.jsx
+++ UniVerseUI/universe-ui/src/components/fallbacks/ErrorFallback.jsx
@@ -1,14 +1,10 @@
-import { useErrorBoundary } from "react-error-boundary"
 import { MdErrorOutline } from "react-icons/md";
 
 const ErrorFallback = ({error}) => {
-  const { resetBoundary } = useErrorBoundary();
-
   return (
     <div className="error-fallback-container">
       <div className='error-fallback'>
-        <span><MdErrorOutline className="error-icon"/> Error: {error.message}</span>
-        <button onClick={resetBoundary} className="confirm-button">Try Again</button>
+        <span><MdErrorOutline className="error-icon"/>{error}</span>
       </div>
     </div>
   )
