e4bd4c4d365a1cba855c8dd0e882024391f37d85
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-16 12:14:59 +0200

    Update vite config to change domain to localhost

--- UniVerseUI/universe-ui/vite.config.js
+++ UniVerseUI/universe-ui/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import dns from 'dns'
 import react from '@vitejs/plugin-react'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
