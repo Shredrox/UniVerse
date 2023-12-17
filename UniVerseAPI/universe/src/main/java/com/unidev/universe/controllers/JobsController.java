7d5fc2998718bf00b1fcb34ffea08ac24b6bbbee
Silvio <stoikov126@gmail.com>
2023-12-17 13:32:42 +0200

    Add getAllJobs method to the JobsController.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/controllers/JobsController.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/controllers/JobsController.java
@@ -1,4 +1,22 @@
 package com.unidev.universe.controllers;
 
+import com.unidev.universe.entities.JobOffer;
+import com.unidev.universe.services.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/universe/jobs")
 public class JobsController {
+    @Autowired
+    private JobService jobService;
+
+    @GetMapping
+    public List<JobOffer> getAllJobs(){
+        return jobService.getAllJobs();
+    }
 }
