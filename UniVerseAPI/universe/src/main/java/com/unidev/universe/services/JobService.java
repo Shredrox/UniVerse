5ae29d30710eb4b892a1ea2c704dbc22ed21c032
Silvio <stoikov126@gmail.com>
2023-12-17 13:19:32 +0200

    Create getAllJobs method in JobService class.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/services/JobService.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/services/JobService.java
@@ -1,4 +1,18 @@
 package com.unidev.universe.services;
 
+import com.unidev.universe.entities.JobOffer;
+import com.unidev.universe.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class JobService {
+    @Autowired
+    private JobRepository jobRepository;
+
+    public List<JobOffer> getAllJobs(){
+        return jobRepository.findAll();
+    }
 }
