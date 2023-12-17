617382ae9a73be01df99c12c8d2a29af0f9d8a8b
Silvio <stoikov126@gmail.com>
2023-12-17 13:35:11 +0200

    Add getJobById method to the JobsController.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/controllers/JobsController.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/controllers/JobsController.java
@@ -3,11 +3,14 @@ package com.unidev.universe.controllers;
 import com.unidev.universe.entities.JobOffer;
 import com.unidev.universe.services.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/universe/jobs")
@@ -18,5 +21,11 @@ public class JobsController {
     @GetMapping
     public List<JobOffer> getAllJobs(){
         return jobService.getAllJobs();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JobOffer> getJobById(@PathVariable Long id){
+        Optional<JobOffer> job = jobService.getJobById(id);
+        return job.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
     }
 }
