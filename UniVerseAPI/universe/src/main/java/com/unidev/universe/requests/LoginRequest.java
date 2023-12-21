90533104650bef88fc33bd233d4608233ee82272
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-21 22:32:10 +0200

    Add login and register requests

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/LoginRequest.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/LoginRequest.java
@@ -1,15 +1,13 @@
-//package com.unidev.universe.requests;
-//
-//import lombok.*;
-//
-//@Data
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class LoginRequest {
-//    private String email;
-//    private String password;
-//    private String name;
-//    private String lastName;
-//}
+package com.unidev.universe.requests;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
--- UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/RegisterRequest.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/RegisterRequest.java
@@ -1,15 +1,14 @@
-//package com.unidev.universe.requests;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class RegisterRequest {
-//    private String name;
-//    private String lastName;
-//    private String email;
-//    private String password;
-//}
+package com.unidev.universe.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+}
