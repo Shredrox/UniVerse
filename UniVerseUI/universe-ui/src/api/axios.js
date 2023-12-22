721a0fa1942e4abd9532723ed1c6a6693826bb8a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-22 13:05:10 +0200

    Add v1 to axios base url

--- UniVerseUI/universe-ui/src/api/axios.js
+++ UniVerseUI/universe-ui/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8080/api/';
+const BASE_URL = 'http://localhost:8080/api/v1';
 
 export default axios.create({
     baseURL: BASE_URL
