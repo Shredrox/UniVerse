2413f4644af4c6861b2f32023d4a60dd5c1ab87e
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-23 11:40:27 +0200

    Update jobsApi with backend endpoints

--- UniVerseUI/universe-ui/src/api/jobsApi.js
+++ UniVerseUI/universe-ui/src/api/jobsApi.js
@@ -1,103 +1,32 @@
 import axios from "./axios"
 
-// export const getJobs = async () =>{
-//     const response = await axios.get('/Jobs/GetAllJobs');
-//     return response.data;
-// }
+export const getJobs = async () =>{
+    const response = await axios.get('/jobs');
+    return response.data;
+}
 
-// export const getJobById = async (id) =>{
-//     return await axios.get(`/Jobs/${id}`, id);
-// }
+export const getJobById = async (id) =>{
+    return await axios.get(`/jobs/${id}`, id);
+}
 
-export const getJobs = () =>{
-    const jobs = [
-        {
-            id: "1",
-            title: "Frontend Developer",
-            company: "Microsoft",
-            type: "Full Time",
-            requirements: "Senior",
-            location: "Plovdiv",
-            datePosted: "3-12-2023",
-            description: "ahgjasngjslgjnrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrlgjnrkbjnlkrkbjnlkrdbnlkdfbdfb"
-        },
-        {
-            id: "2",
-            title: "Test 2",
-            company: "Amazing company",
-            requirements: "Junior",
-            location: "Sofia",
-            datePosted: "3-12-2023",
-            type: "Part Time"
-        },
-        {
-            id: "3",
-            title: "Test 3",
-            company: "MarsBros",
-            requirements: "Mid",
-            location: "Mars",
-            datePosted: "3-12-2023",
-            type: "Internship"
-        },
-        {
-            id: "4",
-            title: "Test 1",
-            requirements: "Senior",
-            location: "Sofia"
-        },
-        {
-            id: "5",
-            title: "Test 2",
-            requirements: "Junior",
-            location: "Sofia"
-        },
-        {
-            id: "6",
-            title: "Test 3",
-            requirements: "Mid"
-        },
-        {
-            id: "7",
-            title: "Test 1",
-            requirements: "Senior"
-        },
-        {
-            id: "8",
-            title: "Test 2",
-            requirements: "Junior"
-        },
-        {
-            id: "9",
-            title: "Test 3",
-            requirements: "Mid"
+export const applyToJob = async ({jobId, username}) =>{
+    const response = await axios.post(`/jobs/${jobId}/apply`, {
+        params: {
+          username: username
         }
-    ]
-
-    return jobs;
-
-    //const response = await axios.get('/Jobs/GetAllJobs');
-    //return response.data;
-}
-
-export const getJobById = (id) =>{
-    const result = getJobs().find(job => job.id === id);
-    return result;
-
-    //return await axios.get(`/Jobs/${id}`, id);
-}
-
-export const applyToJob = ({jobId, username}) =>{
-    //TO DO: Add request and API endpoint
+    });
+    
+    return response.data;
 }
 
 export const addJob = async (job) =>{
-    return await axios.post('/Jobs/CreateJobOffer', job);
+    return await axios.post('/jobs/createJob', job);
 }
 
 export const updateJob = async (job) =>{
-    return await axios.patch(`/Jobs/${job.id}`, job);
+    return await axios.patch(`/jobs/${job.id}`, job);
 }
 
-export const deleteJob = async ({id}) =>{
-    return await axios.delete(`/Jobs/${id}`, id);
+export const deleteJob = async (id) =>{
+    return await axios.delete(`/jobs/${id}`, id);
 }
