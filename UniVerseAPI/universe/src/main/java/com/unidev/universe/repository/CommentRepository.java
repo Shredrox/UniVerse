c0853bf9f8262e1e7ee5de58361b80173247de5f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-23 12:05:06 +0200

    Update CommentRepository

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/repository/CommentRepository.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/repository/CommentRepository.java
@@ -3,6 +3,10 @@ package com.unidev.universe.repository;
 import com.unidev.universe.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Comment findById(int commentId);
+    Optional<List<Comment>> findByPostId(long postId);
+    Optional<List<Comment>> findAllByParentCommentId(long parentCommentId);
 }
