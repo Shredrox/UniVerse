902a9efd0db4df07061122330e97733f6bd3be6c
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-23 11:42:13 +0200

    Add findByTitle to NewsRepository

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/repository/NewsRepository.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/repository/NewsRepository.java
@@ -3,6 +3,8 @@ package com.unidev.universe.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.unidev.universe.entities.News;
 
+import java.util.Optional;
+
 public interface NewsRepository extends JpaRepository<News, Long> {
-    News findById(int newsId);
+    Optional<News> findByTitle(String title);
 }
