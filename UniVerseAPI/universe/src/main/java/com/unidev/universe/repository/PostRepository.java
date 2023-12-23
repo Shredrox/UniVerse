ba5585eedb5db243b83829ef4db72dab19e892c9
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-23 12:06:46 +0200

    Update PostRepository

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/repository/PostRepository.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/repository/PostRepository.java
@@ -3,6 +3,9 @@ package com.unidev.universe.repository;
 import com.unidev.universe.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Post findById(int postId);
+    Post findById(long postId);
+    List<Post> findByAuthorNameIn(List<String> usernames);
 }
