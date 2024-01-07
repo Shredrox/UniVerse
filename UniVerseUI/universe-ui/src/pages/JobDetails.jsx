979ce0804437f278bb5d4d52fbf0687034364182
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-07 14:21:23 +0200

    Refactor JobDetails with useJobData

--- UniVerseUI/universe-ui/src/pages/JobDetails.jsx
+++ UniVerseUI/universe-ui/src/pages/JobDetails.jsx
@@ -1,43 +1,55 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom"
 import { useAuth } from "../hooks/useAuth";
-import { applyToJob, getJobById } from "../services/jobsService";
 import { IoBriefcase } from "react-icons/io5";
 import { FaLocationDot } from "react-icons/fa6";
 import { HiBuildingOffice2 } from "react-icons/hi2";
 import Loading from '../components/fallbacks/Loading'
+import useJobData from "../hooks/useJobData";
 
 const JobDetails = () => {
   const { jobId } = useParams();
   const { auth } = useAuth();
 
-  const {data: job, isLoading, isError, error} = useQuery({ 
-    queryKey: ["job", jobId],
-    queryFn: () => getJobById(jobId),
-  });
+  const { 
+    jobData, 
+    isJobLoading,
+    isJobError,
+    jobError,
+    applyToJobMutation,
+    cancelApplicationToJobMutation
+  } = useJobData(jobId, auth?.user);
 
-  const {mutateAsync: applyToJobMutation} = useMutation({
-    mutationFn: applyToJob,
-  });
+  const handleApply = () =>{
+    if(jobData.isApplied){
+      cancelApplicationToJobMutation({jobId: jobId, username: auth?.user})
+      return;
+    }
 
-  if(isError){
-    throw Error(error);
+    applyToJobMutation({jobId: jobId, username: auth?.user})
   }
 
-  if(isLoading){
+  if(isJobError){
+    throw Error(jobError);
+  }
+
+  if(isJobLoading){
     return <Loading/>
   }
 
   return (
     <div className="job-details-container">
       <div className="job-details">
-        <h2>{job?.title}</h2>
-        <h3>{job?.company}</h3>
-        <span className="job-text-field"><HiBuildingOffice2 className="job-icon"/>{job?.requirements}</span>
-        <span className="job-text-field"><FaLocationDot className="job-icon"/>{job?.location}</span>
-        <span className="job-text-field"><IoBriefcase className="job-icon"/>{job?.type}</span>
-        <p className="job-description">{job?.description}</p>
-        <button onClick={() => applyToJobMutation({jobId: job.id, username: auth?.user})} className="confirm-button">Apply</button>
+        <h2>{jobData.job?.title}</h2>
+        <h3>{jobData.job?.company}</h3>
+        <span className="job-text-field"><HiBuildingOffice2 className="job-icon"/>{jobData.job?.requirements}</span>
+        <span className="job-text-field"><FaLocationDot className="job-icon"/>{jobData.job?.location}</span>
+        <span className="job-text-field"><IoBriefcase className="job-icon"/>{jobData.job?.type}</span>
+        <p className="job-description">{jobData.job?.description}</p>
+        <button 
+        onClick={handleApply} 
+        className="job-confirm-button">
+          {jobData.isApplied ? "Cancel Application" : "Apply"}
+        </button>
       </div>
     </div>
   )
