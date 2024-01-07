49bdb74120ce32353ab6253c2b65da308354eba5
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-07 14:17:56 +0200

    Create useJobData hook

---
+++ UniVerseUI/universe-ui/src/hooks/useJobData.js
@@ -0,0 +1,53 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { applyToJob, cancelApplicationToJob, getIsAppliedToJob, getJobById } from "../services/jobsService";
+
+const useJobData = (jobId, user) =>{
+  const queryClient = useQueryClient();
+
+  const {data: job, 
+    isLoading: isJobDetailsLoading, 
+    isError: isJobDetailsError, 
+    error: jobDetailsError
+  } = useQuery({ 
+    queryKey: ["job", jobId],
+    queryFn: () => getJobById(jobId),
+  });
+
+  const {data: isApplied, 
+    isLoading: isAppliedLoading, 
+    isError: isAppliedError, 
+    error: appliedError
+  } = useQuery({ 
+    queryKey: ["jobIsApplied", jobId, user],
+    queryFn: () => getIsAppliedToJob(jobId, user),
+  });
+
+  const {mutateAsync: applyToJobMutation} = useMutation({
+    mutationFn: applyToJob,
+    onSuccess: () =>{
+      queryClient.invalidateQueries(["jobIsApplied", jobId, user]);
+    },
+  });
+
+  const {mutateAsync: cancelApplicationToJobMutation} = useMutation({
+    mutationFn: cancelApplicationToJob,
+    onSuccess: () =>{
+      queryClient.invalidateQueries(["jobIsApplied", jobId, user]);
+    },
+  });
+
+  const isJobError =  isJobDetailsError || isAppliedError;
+  const jobError = jobDetailsError || appliedError;
+  const isJobLoading = isJobDetailsLoading || isAppliedLoading;
+
+  return {
+    jobData: {job, isApplied}, 
+    isJobLoading, 
+    isJobError, 
+    jobError,
+    applyToJobMutation,
+    cancelApplicationToJobMutation
+  }
+}
+
+export default useJobData
