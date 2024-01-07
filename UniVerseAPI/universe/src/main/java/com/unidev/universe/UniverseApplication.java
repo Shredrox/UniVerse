b4030c58c28561a59bc90b3ac16c2a9f45351d5d
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-07 23:43:09 +0200

    Remove unneeded app imports

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/UniverseApplication.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/UniverseApplication.java
@@ -1,12 +1,8 @@
 package com.unidev.universe;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
