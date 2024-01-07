1bbd374ae9dadfd6833b27da243dab4d1e5b889f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-07 15:33:54 +0200

    Add chat user profile picture to ChatCard

--- UniVerseUI/universe-ui/src/components/ChatCard.jsx
+++ UniVerseUI/universe-ui/src/components/ChatCard.jsx
@@ -1,5 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom"
 import { useAuth } from '../hooks/useAuth'
+import { FaUserAstronaut } from "react-icons/fa";
+import useProfilePicture from "../hooks/useProfilePicture";
 
 const ChatCard = ({chat}) => {
   const location = useLocation();
@@ -8,6 +10,8 @@ const ChatCard = ({chat}) => {
   const { auth } = useAuth();
   const chatUser = auth?.user === chat.user1 ? chat.user2 : chat.user1;
   const chatPath = `/chats/${chatUser}`;
+
+  const { profilePicture } = useProfilePicture("chatUserProfilePicture", chatUser);
 
   function isSelected(path){
     return location.pathname.includes(path) ? 'chat-card-active' : 'chat-card'
@@ -18,7 +22,12 @@ const ChatCard = ({chat}) => {
       <div className='chat-card-line'>&nbsp;</div>
       <div className={isSelected(chatPath)}  
         onClick={() => navigate(chatPath)}>
-        <div className="chat-profile-picture"></div>
+        <div className="chat-profile-picture">
+          {profilePicture?.size > 0 ? 
+          <img className='author-profile-picture' src={URL.createObjectURL(profilePicture)} alt="ProfilePicture" /> 
+          :
+          <FaUserAstronaut className="chat-profile-picture-placeholder-icon"/>}
+        </div>
         {chatUser}
       </div>
     </div>
