879acd5ca8b66eb61e561c879603efc7f943a53f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-08 11:45:57 +0200

    Create useFeedData hook

---
+++ UniVerseUI/universe-ui/src/hooks/useFeedData.js
@@ -0,0 +1,46 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { addPost, getFriendsPosts } from "../services/postsService";
+
+const useFeedData = (user) => {
+  const queryClient = useQueryClient();
+
+  const fetchAndSortPosts = async () => {
+    try {
+      const data = await getFriendsPosts(user);
+      const sortedPosts = data.slice().sort((a, b) => {
+        const dateA = new Date(a.timestamp).getTime();
+        const dateB = new Date(b.timestamp).getTime();
+        return dateB - dateA;
+      });
+      return sortedPosts;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  const {data: posts, 
+    isLoading: isPostsLoading, 
+    isError: isPostsError, 
+    error: postsError
+  } = useQuery({ 
+    queryKey: ["posts", user],
+    queryFn: fetchAndSortPosts
+  });
+
+  const {mutateAsync: addPostMutation} = useMutation({
+    mutationFn: addPost,
+    onSuccess: () =>{
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
+
+  return {
+    posts, 
+    isPostsLoading, 
+    isPostsError, 
+    postsError,
+    addPostMutation
+  }
+}
+
+export default useFeedData
