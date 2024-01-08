e9dd0e3fd437ca822d1ea2e45b87f52bf79eb507
Valentin Hadzhiminov <35624036+ValentinH1810@users.noreply.github.com>
2024-01-08 21:49:53 +0200

    Revert "Update FriendshipControllerTest."
    This reverts commit c47f0995cb61379f246a2eee23a959a7a7ff6fcc.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/FriendRequest.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/requests/FriendRequest.java
@@ -1,12 +1,8 @@
 package com.unidev.universe.requests;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class FriendRequest {
     private String sender;
     private String receiver;
