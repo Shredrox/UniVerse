2a2acddda4b6e897aed8d43e3ccb972884aa9c2b
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-08 20:23:47 +0200

    Remove Component tag

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/config/JwtAuthenticationFilter.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/config/JwtAuthenticationFilter.java
@@ -12,12 +12,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
