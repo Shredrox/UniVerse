23a037f322b1da6698e000c38a27db207dbc6a68
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-08 20:23:58 +0200

    Add AutoConfigureMockMvc(addFilters = false) tag

--- UniVerseAPI/universe/src/test/java/com/unidev/universe/controllertests/PostControllerTest.java
+++ UniVerseAPI/universe/src/test/java/com/unidev/universe/controllertests/PostControllerTest.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PostController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class PostControllerTest {
 
     @Autowired
