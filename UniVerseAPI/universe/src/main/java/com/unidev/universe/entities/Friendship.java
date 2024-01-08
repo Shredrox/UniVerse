9170cb177bb706ba58d39136fc4f1cc1b3254df5
Valentin Hadzhiminov <35624036+ValentinH1810@users.noreply.github.com>
2024-01-08 21:50:45 +0200

    Revert "Create FriendshipControllerTest."
    This reverts commit e2e69d48f6b01027e1392db2803eee8c2f68d2f6.

--- UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/Friendship.java
+++ UniVerseAPI/universe/src/main/java/com/unidev/universe/entities/Friendship.java
@@ -12,8 +12,8 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "friendships")
 public class Friendship {
     @Id
